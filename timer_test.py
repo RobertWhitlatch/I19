1dfe7ed9bfc165b9c6a1557333ff2ac627ee8742
Mitchell <whitlatch@coherentlogix.com>
2019-04-19 11:47:34 -0500

    s

--- timer_test.py
+++ timer_test.py
@@ -1,13 +1,19 @@
 # importing the required module 
 import timeit 
 
+def usleep(delay):
+   mdelay = delay /1000
+   now = time.time()
+   while now + mdelay > time.time():
+      pass
+
 # code snippet to be executed only once
 setup = "from time import sleep"
 
 # code snippet whose execution time is to be measured
 code = ''' 
 def example(): 
-    sleep(1.0/1000.0)
+    usleep(50)
 '''
 
 # timeit statement
