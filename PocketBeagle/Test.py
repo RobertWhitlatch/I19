63824f444c69abd440e26e7d38f8b327ab89a050
Mitchell <robert.whitlatch@utexas.edu>
2019-05-01 14:43:52 -0500

    v

--- PocketBeagle/Test.py
+++ PocketBeagle/Test.py
@@ -1,20 +1,34 @@
 from MotorLibrary import *
 
-depot = MotorDepot()
+depot = MotorDepot(1000, 200)
 
-brushed = depot.get_group(0, 'Brushed')
-zero = brushed[0]
-one = brushed[1]
-
+bridged = depot.get_group(0, 'Bridged')
+bridged_0 = bridged[0]
+bridged_0.set_all(2048, True)
+bridged = depot.get_group(1, 'Bridged')
+bridged_1 = bridged[0]
+bridged_1.set_all(4096, True)
+brushed_0, brushed_1 = depot.get_group(7, 'Brushed')
+brushed_0.set_all(2048, True)
+brushed_1.set_all(4096, True)
+servo_0, servo_1, servo_2, servo_3 = depot.get_group(6, 'Servo')
+stepper = depot.get_group(3, 'Stepper')
+stepper_0 = stepper[0]
 while True:
+    servo_0.set_position(410)
+    stepper_0.move_num_steps(20, 'CW')
+    servo_1.set_position(410)
+    stepper_0.move_num_steps(20, 'CW')
+    servo_2.set_position(410)
+    stepper_0.move_num_steps(20, 'CW')
+    servo_0.set_position(2048)
+    stepper_0.move_num_steps(20, 'CW')
+    servo_1.set_position(2048)
+    stepper_0.move_num_steps(20, 'CW')
+    servo_2.set_position(2048)
+    stepper_0.move_num_steps(20, 'CW')
 
-    input('Press any key to start group 0...')
 
-    zero.set_all(2048, 'forward', 'fast')
-    one.set_all(2048, 'forward', 'fast')
 
-    input('Press any key to stop group 0...')
 
-    zero.set_speed(0)
-    one.set_speed(0)
 
